4d1ee4adf14911e3d4d3f49eb1510579fee9d423
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-26 02:32:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,65 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @species = species
+    @@all << self
+    
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def species
+    @species = "human"
+  end 
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.count
+    @@all.size 
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats 
+    
+  end
+  
+  def dogs 
+    
+  end
+  
+  def buy_cat
+    
+  end
+  
+  def buy_dog
+    
+  end
+  
+  def walk_dogs
+    
+  end
+  
+  def feed_cats
+    
+  end
+  
+  def sell_pets
+    
+  end
+  
+  def list_pets
+    
+  end
+  
 end
